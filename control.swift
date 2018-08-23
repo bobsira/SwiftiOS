e32af957046d359aeb4ce254f2f2c3a45e1e52ae
bobsira <sbobfitz2@gmail.com>
2018-08-23 13:03:57 +0300

    modified the flow control doc

--- control.swift
+++ control.swift
@@ -1,4 +1,4 @@
-import Foundation 
+import Foundation
 
 let names = ["Anna", "Alex", "Brian", "Jack"]
 for name in names {
@@ -17,9 +17,9 @@ for index in 1...5 {
 
 let base = 3
 let power = 10
-var answer = 1 
+var answer = 1
 for _ in 1...power {
-    answer *= base 
+    answer *= base
 }
 print("\(base) to the power of \(power) is \(answer)")
 
@@ -28,7 +28,7 @@ for tickMark in 0..<minutes {
     // render the tick mark each minute (60 times)
 }
 
-let minuteInterval = 5 
+let minuteInterval = 5
 for tickMark in stride(from: 0, to: minutes, by: minuteInterval) {
     // render the tick mark every 5 minutes (0,5,10,15 ... 45, 50, 55)
 }
@@ -39,16 +39,16 @@ for tickMark in stride(from: 3, through: hours, by: hourInterval){
     // render the tick mark every 3 hours (3, 6, 9, 12)
 }
 
-let finalSquare = 25 
+let finalSquare = 25
 var board = [Int](repeating: 0, count: finalSquare + 1)
 
 board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
-board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08 
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
 
 var square = 0
 var diceRoll = 0
 while square < finalSquare {
-    // roll the dice 
+    // roll the dice
     diceRoll += 1
     if diceRoll == 7 { diceRoll = 1 }
     // move by the rolled amount
@@ -61,12 +61,190 @@ while square < finalSquare {
 print("Game over!")
 
 repeat {
-    // move up or down for a snake or ladder 
+    // move up or down for a snake or ladder
     square += board[square]
-    // roll the dice 
+    // roll the dice
     diceRoll += 1
     if diceRoll == 7 { diceRoll = 1}
-    //move by the rolled amount 
+    //move by the rolled amount
     square += diceRoll
 } while square < finalSquare
 print("Game over!")
+
+let someCharacter: Character = "z"
+switch someCharacter {
+    case "a":
+        print("The first letter of the alphabet")
+    case "z":
+        print("The last letter of the alphabet")
+    default:
+        print("Some other character")
+}
+
+//invalid code below
+let anotherCharacter: Character = "a"
+switch anotherCharacter {
+   // case "a": // Invalid, the case has an empty body
+   // case "A":
+   case "a", "A":
+        print("The letter A")
+    default:
+        print("Not the letter A")
+}
+//this will report a compile-time error
+
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+let naturalCount: String
+switch approximateCount {
+    case 0:
+        naturalCount = "no"
+    case 1..<5:
+        naturalCount = "a few"
+    case 5..<12:
+        naturalCount = "several"
+    case 12..<100:
+        naturalCount = "dozens of"
+    case 100..<1000:
+        naturalCount = "hundreds of"
+    default:
+        naturalCount = "many"
+}
+print("There are \(naturalCount) \(countedThings).")
+
+let somePoint = (1,1)
+switch somePoint {
+    case (0,0):
+        print("\(somePoint) is at the origin")
+    case (_,0):
+        print("\(somePoint) is on the x-axis")
+    case (0, _):
+        print("\(somePoint) is on the y-axis")
+    case (-2...2, -2...2):
+        print("\(somePoint) is inside the box")
+    default:
+        print("\(somePoint) is outside of the box")
+}
+
+let anotherPoint = (2,0)
+switch anotherPoint {
+    case (let x, 0):
+        print("on the x-axis with an x value of \(x)")
+    case (0, let y):
+        print("on the y-axis with a y value of \(y)")
+    case let(x, y):
+        print("somewhere else at (\(x), \(y))")
+}
+
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+    case let (x,y) where x == y:
+        print("(\(x), \(y)) is on the line x == y")
+    case let (x, y) where x == -y:
+        print("(\(x), \(y)) is on the line x == -y")
+    case let (x, y):
+        print("(\(x), \(y)) is just some arbitrary point")
+}
+
+let someCharacter1: Character = "e"
+switch someCharacter1 {
+    case "a", "e", "i", "o", "u":
+        print("\(someCharacter1) is a vowel")
+    case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+         "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
+        print("\(someCharacter1) is a consonant")
+    default:
+        print("\(someCharacter1) is not a vowel or a consonant")
+}
+
+let stillAnotherPoint = (9, 0)
+switch stillAnotherPoint {
+    case (let distance, 0), (0, let distance):
+        print("On an axis, \(distance) from the origin")
+    default:
+        print("Not on an axis")
+}
+
+let puzzleInput = "great minds think alike"
+var puzzleOutput = ""
+let charactersToRemove: [Character] = ["a", "e", "i", "o", "u", " "]
+for character in puzzleInput {
+    if charactersToRemove.contains(character){
+        continue
+    } else {
+        puzzleOutput.append(character)
+    }
+}
+print(puzzleOutput)
+
+let numberSymbol: Character = "三"  // Chinese symbol for the number 3
+var possibleIntegerValue: Int?
+switch numberSymbol {
+    case "1", "١", "一", "๑":
+        possibleIntegerValue = 1
+    case "2", "٢", "二", "๒":
+        possibleIntegerValue = 2
+    case "3", "٣", "三", "๓":
+        possibleIntegerValue = 3
+    case "4", "٤", "四", "๔":
+        possibleIntegerValue = 4
+    default:
+        break
+}
+if let integerValue = possibleIntegerValue {
+    print("The integer value of \(numberSymbol) is \(integerValue).")
+} else {
+    print("An integer value could not be found for \(numberSymbol).")
+}
+
+let integerToDescribe = 5
+var description = "The number \(integerToDescribe) is"
+switch integerToDescribe {
+    case 2, 3, 5, 7, 11, 13, 17, 19:
+        description += " a prime number, and also"
+        fallthrough
+    default:
+        description += " an integer."
+}
+print(description)
+
+let finalSquare = 25
+var board = [Int](repeating: 0, count: finalSquare + 1)
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+var square = 0
+var diceRoll = 0
+
+gameLoop: while square != finalSquare {
+    diceRoll += 1
+    if  diceRoll == 7 { diceRoll = 1}
+    switch square + diceRoll {
+        case finalSquare:
+            // diceRoll will move us to the final square, so the game is over
+            break gameLoop
+        case let newSquare where newSquare > finalSquare:
+            // diceRoll will move us beyond the final square, so roll again
+            continue gameLoop
+        default:
+            // this is a valid move, so find out its effect
+            square += diceRoll
+            square += board[square]
+    }
+}
+print("Game Over")
+
+func greet(person: [String: String] ) {
+    guard let name = person["name"] else {
+        return
+    }
+    print("Hello \(name)!")
+
+    guard let location = person["location"]  else {
+        print("I hope the weather is nice near you.")
+        return
+    }
+    print("I hope the weather is nice in \(location).")
+}
+
+greet(person: ["name": "John"])
+greet(person: ["name": "Jane", "location": "Cupertino"])
