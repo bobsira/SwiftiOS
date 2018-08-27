7f3615841e336f11222f6803be59f961fc1236cc
bobsira <sbobfitz2@gmail.com>
2018-08-27 19:26:11 +0300

    structures and classes swift

---
+++ class_structure.swift
@@ -0,0 +1,69 @@
+struct SomeStructure {
+  // structure definition goes here
+}
+
+class SomeClass {
+  // class definition goes here
+}
+
+struct Resolution {
+  var width = 0
+  var height = 0
+}
+
+class VideoMode {
+  var resolution = Resolution()
+  var interlaced = false
+  var frameRate = 0.0
+  var name: String?
+}
+
+let someResolution = Resolution()
+let someVideoMode = VideoMode()
+
+print("The width of someResolution is \(someResolution.width)")
+print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+
+someVideoMode.resolution.width = 1280
+print("The width of someVideoMode is now \(someVideoMode.resolution.width)")
+
+
+let vga = Resolution(width: 640, height: 480)
+
+
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+cinema.width = 2048
+print("cinema is now \(cinema.width) pixel wide") // 2048
+print("hd is still \(hd.width) pixels wide")
+
+
+enum CompassPoint {
+  case north, south, east, west
+  mutating func turnNorth() {
+    self = .north
+  }
+}
+var currentDirection = CompassPoint.west
+let rememberedDirection = currentDirection
+currentDirection.turnNorth()
+print("The current direction is \(currentDirection)")
+print("The remembered direction is \(rememberedDirection)")
+// Prints "The current direction is north"
+// Prints "The remembered direction is west"
+
+
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080i"
+tenEighty.frameRate = 25.0
+let alsoTenEighty = tenEighty
+alsoTenEighty.frameRate = 30.0
+
+print("The frameRate property of tenEighty is now \(tenEighty.frameRate)")
+
+
+if tenEighty === alsoTenEighty {
+  print("tenEighty and alsoTenEighty refer to the same VideoMode instance.")
+}
