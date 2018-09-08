120193705f1a6091955cc33cf115c3cfb40247a1
bobsira <bob.sira@betika.com>
2018-09-08 12:41:03 +0300

    tried playing wav file and failured terribly

--- RetroCalc/RetroCalc/ViewController.swift
+++ RetroCalc/RetroCalc/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var btnSound: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+//        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+//        let soundURL = URL(fileURLWithPath: path!)
+//        do{
+//            try btnSound! = AVAudioPlayer(contentsOf: soundURL)
+//        } catch let error as NSError {
+//            print(error.debugDescription)
+//        }
+    }
+    
+    @IBAction func numberPressed(sender:UIButton){
+        //playSound()
+    }
+    
+    func playSound(){
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        btnSound.play()
     }
 
     override func didReceiveMemoryWarning() {
