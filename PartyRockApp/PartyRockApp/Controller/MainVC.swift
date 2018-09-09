38b5626ba37f2b84aa24e4b69e7bdd1b1abff65a
bobsira <bob.sira@betika.com>
2018-09-09 14:11:45 +0300

    designing ui of party rock app

---
+++ PartyRockApp/PartyRockApp/Controller/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  MainVC.swift
+//  PartyRockApp
+//
+//  Created by bobsirasira on 09/09/2018.
+//  Copyright © 2018 bobsirasira. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
