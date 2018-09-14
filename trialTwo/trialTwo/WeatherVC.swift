3e026d842fa458b48222d892112210a539239ffe
bobsira <sbobfitz2@gmail.com>
2018-09-14 20:53:32 +0300

    completed ui and connected iooutlets

--- trialTwo/trialTwo/WeatherVC.swift
+++ trialTwo/trialTwo/WeatherVC.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class WeatherVC: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var currentTempLabel: UILabel!
+    
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
