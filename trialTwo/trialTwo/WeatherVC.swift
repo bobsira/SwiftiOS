36156f8424ad8e9f999e0eda79b43fc7aefd4ead
bobsira <sbobfitz2@gmail.com>
2018-09-14 21:11:30 +0300

    updates on the ui

--- trialTwo/trialTwo/WeatherVC.swift
+++ trialTwo/trialTwo/WeatherVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var dateLabel: UILabel!
     
@@ -21,12 +21,28 @@ class WeatherVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+        
+        return cell
     }
 
 
