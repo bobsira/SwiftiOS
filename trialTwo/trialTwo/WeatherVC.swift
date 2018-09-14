e55ea927078d253d147aa4b8fcb5c157c8a8dacb
bobsira <sbobfitz2@gmail.com>
2018-09-14 20:19:09 +0300

    created ui for the app

--- trialTwo/trialTwo/WeatherVC.swift
+++ trialTwo/trialTwo/WeatherVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherVC.swift
 //  trialTwo
 //
 //  Created by bobsirasira on 14/09/2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
