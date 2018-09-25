82fead5823afe3b0727ff042e6a2033e28d86e99
bobsira <sbobfitz2@gmail.com>
2018-09-25 19:13:47 +0300

    convert to mvc design pattern

---
+++ Todoey/Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by bobsirasira on 25/09/2018.
+//  Copyright © 2018 bobsirasira. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
