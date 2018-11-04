470222717895b6544ba0b3150c3856391ed45d4e
sirasira <bob.sira@betika.com>
2018-11-04 12:10:38 +0300

    expandable and collapsible sections uitableview

---
+++ UITableViewAdvance/UITableViewAdvance/ExpandableNames.swift
@@ -0,0 +1,16 @@
+//
+//  ExpandableNames.swift
+//  UITableViewAdvance
+//
+//  Created by bobsirasira on 04/11/2018.
+//  Copyright © 2018 bobsirasira. All rights reserved.
+//
+
+import Foundation
+
+struct ExpandableNames {
+    
+    var isExpanded: Bool
+    let names: [String]
+    
+}
