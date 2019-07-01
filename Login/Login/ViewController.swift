798ff37242273519e7ec7ade4eb9cedda5685e75
sirasira <bob.sira@betika.com>
2019-07-01 17:00:53 +0300

    add programmatic segues

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -11,14 +11,38 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var logIn: UIButton!
+    @IBOutlet weak var forgotUserName: UIButton!
+    @IBOutlet weak var forgotPassword: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = userName.text
+        guard let sender = sender as? UIButton else {
+            return
+        }
+        if sender == forgotPassword {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else if sender == forgotUserName {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = userName.text
+        }
     }
-
+    
+    @IBAction func logIn(_ sender: Any) {
+        performSegue(withIdentifier: "Landing", sender: logIn)
+    }
+    
+    @IBAction func forgotUserName(_ sender: Any) {
+        performSegue(withIdentifier: "Landing", sender: forgotUserName)
+    }
+    @IBAction func forgotPassword(_ sender: Any) {
+        performSegue(withIdentifier: "Landing", sender: forgotPassword)
+    }
+    
 }
 
