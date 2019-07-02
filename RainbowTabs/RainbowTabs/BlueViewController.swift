7a568845f65efea62a36ac24662197cd5fbf5de1
sirasira <bob.sira@betika.com>
2019-07-02 11:34:29 +0300

    iOS Tab Bars justice

---
+++ RainbowTabs/RainbowTabs/BlueViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BlueViewController.swift
+//  RainbowTabs
+//
+//  Created by bobsirasira on 02/07/2019.
+//  Copyright © 2019 bobsirasira. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by bobsirasira on 01/07/2019.
+//  Copyright © 2019 bobsirasira. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tabBarItem.badgeValue = "!"
+    }
+
+
+}
+
